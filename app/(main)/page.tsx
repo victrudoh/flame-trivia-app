60e942b48a22f555cb2385f85d299a1625d194cf
Udoh Victory <victrudoh@gmail.com>
2024-09-11 05:43:29 +0100

    UPDATE: Tailwind working now

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const Page = () => {
   return (
     <>
-      <div className="bg-red-300 text-brand-secondary min-h-screen flex items-center justify-center text-lg font-geistmono p-4">
+      <div className="bg-brand-main text-brand-secondary min-h-screen flex items-center justify-center text-lg font-geistmono p-4">
         Flame of Hope
       </div>
     </>
