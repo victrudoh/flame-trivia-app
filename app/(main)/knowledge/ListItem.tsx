5341207c41203b492f1bc04d7fe20f70a44c33b2
Udoh Victory <victrudoh@gmail.com>
2024-09-11 07:52:04 +0100

    UPDATE: working on knowledge base page.

--- app/(main)/knowledge/ListItem.tsx
+++ app/(main)/knowledge/ListItem.tsx
@@ -8,15 +8,17 @@ import Link from "next/link";
 const ListItem = () => {
   return (
     <>
-      <div className="w-[90%] p-4 border-brand-grayish/20 border-[0.5px] rounded-xl shadow-sm flex items-center justify-between">
-        <div className="w-[58px] h-[58px]">
-          <Image src={img} alt="img" className="rounded-lg" />
-        </div>
-        <div className="flex flex-col gap-2 items-start justify-between text-brand-dark">
-          <span className="font-bold text-base">About HIV/AIDS</span>
-          <span className="font-normal text-xs">
-            HIV is the human Lorem ip...
-          </span>
+      <div className="w-full p-4 border-brand-grayish/20 border-[0.5px] rounded-xl shadow-sm flex items-center justify-between">
+        <div className="flex items-center gap-4">
+          <div className="w-[58px] h-[58px]">
+            <Image src={img} alt="img" className="rounded-lg" />
+          </div>
+          <div className="flex flex-col gap-2 items-start justify-between text-brand-dark">
+            <span className="font-bold text-base">About HIV/AIDS</span>
+            <span className="font-normal text-xs">
+              HIV is the human Lorem ip...
+            </span>
+          </div>
         </div>
         <Link
           href={`/knowledge/12`}
