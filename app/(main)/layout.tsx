b8558ab74586e4b8a6a3a28f46f4004a66fb262b
Udoh Victory <victrudoh@gmail.com>
2024-09-11 09:32:08 +0100

    UPDATE: working on mobile nav footer opacity

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -4,7 +4,7 @@ import MobileNav from "@/components/mobileNav/page";
 export default async function MainLayout({ children }: never) {
   return (
     <div className="flex flex-col min-h-screen">
-      <div className="flex-grow w-full bg-brand-main pb-[80px]">{children}</div>
+      <div className="flex-grow w-full bg-brand-main pb-[40px]">{children}</div>
       <MobileNav />
     </div>
   );
