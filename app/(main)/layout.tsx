a150070a6a44fd438b6cb48298f42817c5ce4c28
Udoh Victory <victrudoh@gmail.com>
2024-09-11 04:54:07 +0100

    UPDATE: Trying to deploy

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,14 +1,9 @@
 import "@/app/globals.css";
 
-// import Header from "./Header";
-// import Footer from "./Footer";
-
 export default async function MainLayout({ children }: never) {
   return (
     <div className="flex flex-col">
-      {/* <Header /> */}
       <div className="bg-brand-ash w-full min-h-screen">{children}</div>
-      {/* <Footer /> */}
     </div>
   );
 }
--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import React from "react";
-// import Link from "next/link";
-// import Image from "next/image";
 
 const Page = () => {
   return (
