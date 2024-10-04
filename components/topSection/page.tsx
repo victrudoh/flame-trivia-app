6a945e5a59ef70f8d5f3cce4b8306db6f8cdf209
Udoh Victory <victrudoh@gmail.com>
2024-10-04 16:33:37 +0100

    UPDATE: Working on trivia card

--- components/topSection/page.tsx
+++ components/topSection/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const TopSection = ({ children }: any) => {
   return (
     <>
-      <div className="glass-fx h-16 w-[98%] rounded-xl flex items-center justify-center sticky top-0 z-10 ">
+      <div className="gradient-fx h-16 w-[98%] rounded-xl flex items-center justify-center sticky top-0 z-10 ">
         {children}
       </div>
     </>
