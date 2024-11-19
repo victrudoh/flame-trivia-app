dfbecbc359031519abd81ee510c749d68ac50f04
Udoh Victory <victrudoh@gmail.com>
2024-11-19 06:08:10 +0100

    UPDATE: Changed splash screen logo stuff

--- app/(landing)/splash/page.tsx
+++ app/(landing)/splash/page.tsx
@@ -19,7 +19,7 @@ const slideTexts = [
   {
     title: "Welcome to Health9ja",
     subtitle:
-      "A youth-friendly online One-Stop-Shop for health information, exciting quiz challenge, counseling, support and referral services at your fingers -tip.",
+      "A youth-friendly online One-Stop-Shop for health information, exciting quiz challenge, counseling, support and referral services at your finger-tips.",
   },
   {
     title: "Explore Cool Health Tips",
